e9044494af647cc3ae3c779b03501cfaf63573c6
banana-piecake <pea0019@balwynhs.vic.edu.au>
2025-08-18 09:12:50 +1000

    added a mod items register and add it to on intialise

--- src/main/java/blimbo.java
+++ src/main/java/blimbo.java
@@ -1,10 +1,12 @@
 package blimbo;
 
+import blimbo.items.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class blimbo implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ModItems.registerModItems();
     }
 }
---
+++ src/main/java/blimbo/items/ModItems.java
@@ -0,0 +1,15 @@
+package blimbo.items;
+
+import net.minecraft.item.Item;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static Item registeritem(String name, Function<Item.Settings, Item> factory, Item.Settings settings){
+
+    }
+    public static void registerModItems() {
+
+    }
+}
