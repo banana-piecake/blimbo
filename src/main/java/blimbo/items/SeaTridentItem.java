13807fea3cc089e79b6a6e2571ac7bf3c5b2e5b7
banana-piecake <pea0019@balwynhs.vic.edu.au>
2025-08-25 21:16:45 +1000

    added a new seatridentitem methox to boost player upwards on use

---
+++ src/main/java/blimbo/items/SeaTridentItem.java
@@ -0,0 +1,21 @@
+package blimbo.items;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public class SeaTridentItem extends Item {
+
+    public SeaTridentItem(Item.Settings settings) {
+        super(settings);
+    }
+
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        Vec3d movementVector = new Vec3d(0, 5, 0);
+        user.travel(movementVector);
+        return ActionResult.SUCCESS;
+    }
+}
